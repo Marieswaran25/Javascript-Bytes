da9884ee7b722d83eae45589af234cf69da20203
marieswaran-prenetics <marieswaran.b@prenetics.com>
2023-03-15 01:03:36 +0530

    flames and spread operator

---
+++ Bytes/flames.js
@@ -0,0 +1,54 @@
+const showResult=(num)=>{
+    if(num==0){
+        console.log('Empty names or both or same names')
+    }
+    else{
+      const flames='FLAMES';
+      const flamesArray=flames.split('')
+      console.log(num)
+    // console.log(flamesArray);
+    for(var i=1;i<=num;i++){
+      for(var j=i;j<=i;j++){
+        if(j==num && (num>=7 && num<=14 && num%2!=0) ){
+          console.log(Math.floor(num/2))
+          flamesArray.splice(Math.floor(num/2),1)
+        }
+        if(j==num && (num>=7 && num<=14 && num%2==0) ){
+          console.log(num%2)
+          flamesArray.splice(num/2,1)
+        }
+      }
+      
+    }
+    console.log(flamesArray);
+
+    }
+
+}
+showResult(11)
+showResult(13)
+showResult(12)
+showResult(9)
+
+// const getNames = (name1, name2) => {
+//   const newArray = [];
+//   const array1 = name1.split("");
+//   const array2 = name2.split("");
+//   array1.map((value) => {
+//     if (array2.includes(value)) {
+//       var indexByValue = array2.indexOf(value);
+//       array2.splice(indexByValue,1);
+//     } else {
+//       newArray.push(value);
+//     }
+//   });
+//   const finalarray = [...newArray, ...array2];
+//   console.log(finalarray);
+//   console.log(finalarray.length);
+// showResult(finalarray.length)
+// };
+// getNames("marieswaran", "venkat");
+// getNames("", "");
+// getNames("aaa", "aaaa");
+// getNames("maayaax", "baacab");
+
---
+++ Bytes/spreadOperator.js
@@ -0,0 +1,5 @@
+const array=[1,2,4,4,2,3];
+const [a,,...rest]=array;
+console.log(...array)
+console.log(a)
+console.log(rest)
