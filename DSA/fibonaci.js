dd9fee2420e53c14cd59d158a96b7f42795a40c8
marieswaran-prenetics <marieswaran.b@prenetics.com>
2023-03-29 20:33:28 +0530

    fibonacci modified

--- DSA/fibonaci.js
+++ DSA/fibonaci.js
@@ -1,26 +1,20 @@
 function fibonaci(n) {
   let n3;
-  let n1;
-  let n2;
+  let n1=0;
+  let n2=1;
   let sum = 0;
-  for (let i = 0; i < n; i++) {
-    if (i == 0) {
-      n2 = 0;
-      n1 = 0;
-    }
-    if (i == 1) {
-      n1 = 0;
-      n2 = 1;
-    }
+  const fib=[n1,n2]
+  for (let i = 2; i < n; i++) {
     n3 = n2 + n1;
     n1 = n2;
     n2 = n3;
     sum += n3;
-    console.log(n3);
-  }
+    fib.push(n3)
+ }
   console.log(`The total sum of fibonacci series of ${n} is ${sum}`);
+  return fib;
 }
-fibonaci(10);
+console.log(fibonaci(10));
 
 function fibonacci2(n){
     const fib=[0,1];
